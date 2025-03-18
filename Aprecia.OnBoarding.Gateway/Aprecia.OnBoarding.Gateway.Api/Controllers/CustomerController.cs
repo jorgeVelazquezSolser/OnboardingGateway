5e2abefa7d1ad0ffaf30b76a9da742fb4731353e
hguerrerog12 <hguerrerog12@hotmail.com>
2025-03-18 09:42:05 -0600

    Requerimiento: Valida Persona Desarrollador: Irving Guerrero Descripción:  Se cambia el nombre del método de People a Customer Comentarios:

--- Aprecia.OnBoarding.Gateway/Aprecia.OnBoarding.Gateway.Api/Controllers/CustomerController.cs
+++ Aprecia.OnBoarding.Gateway/Aprecia.OnBoarding.Gateway.Api/Controllers/CustomerController.cs
@@ -2,11 +2,8 @@
 using Aprecia.Domain.Gateway.People.Dto.Request;
 using Aprecia.Domain.Gateway.People.Dto.Response;
 using Aprecia.Domain.Gateway.People.Media;
-using Aprecia.Domain.Gateway.SalesExecutive.Dtos;
-using Aprecia.Domain.Gateway.SalesExecutive.Media;
 using Aprecia.Domain.Gateway.Shared.Dtos.Request;
 using Aprecia.OnBoarding.Gateway.Api.Configurations;
-using Aprecia.OnBoarding.Gateway.SalesExecutive.Query;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Aprecia.OnBoarding.Gateway.Api.Controllers;
@@ -14,7 +11,7 @@ namespace Aprecia.OnBoarding.Gateway.Api.Controllers;
 [Route("api/[controller]")]
 [ApiController]
 [Authorization]
-public class PeopleController : MediatoRController
+public class CustomerController : MediatoRController
 {
     [HttpPost("Validate")]
     public async Task<IActionResult> ValidatePerson(ResourceRequestBaseDto<DataValidatePersonRquestDto> request)
